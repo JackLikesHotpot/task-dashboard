657fd8b7367dfba178161f15a07e6c1cb305a9e5
JackLikesHotpot <47116970+JackLikesHotpot@users.noreply.github.com>
2025-04-08 02:43:55 +0100

    completed test cases for api

--- backend/__tests__/taskRoutes.test.ts
+++ backend/__tests__/taskRoutes.test.ts
@@ -34,37 +34,35 @@ jest.mock('@prisma/client', () => {
             createdAt: new Date('2020-01-01')
           }
         ]),
-        create: jest.fn().mockResolvedValue([
-          {
+        create: jest.fn().mockResolvedValue({
             id: 1,
             title: 'new task',
             description: 'new description',
             status: 'TO_DO',
             dueDate: new Date('2025-12-31'),
             createdAt: new Date('2020-01-01')
-          }
-        ]),
-        update: jest.fn(),
-        deleteMany: jest.fn(),
-        findUnique: jest.fn().mockResolvedValue([
-        { 
+          }),
+        update: jest.fn().mockResolvedValue({
+          id: 999,
+          title: 'changed name',
+          description: 'changed desc',
+          status: 'COMPLETED',
+          dueDate:  new Date('2025-01-01'),
+          createdAt: new Date('2020-01-01'),
+        }),
+        delete: jest.fn(),
+        findUnique: jest.fn().mockResolvedValue({ 
           id: 999, 
           title: 'completed task', 
           description: 'desc', 
           status: 'COMPLETED', 
           dueDate:  new Date('2025-01-01'),
           createdAt: new Date('2020-01-01')
-        }
-      ]),
-        $disconnect: jest.fn()
+        }),
     },
     }))
   }
 })
-
-const cleanup = async () => {
-  await prisma.task.deleteMany();
-};
 
 const server = express();
 server.use(express.json())
@@ -105,46 +103,28 @@ const generateSampleData = async () => {
   })
 }
 
-describe('Test GET API calls', () => {
-  beforeAll(async () => {
-    await cleanup();
-  })
+describe('Test API calls', () => {
 
   beforeEach(async () => {
     await generateSampleData();
   });
 
-  afterEach(async () => {
-    await cleanup();
-  });
-
-  it('should return all tasks', async () => {
+  it('should GET all tasks', async () => {
     const res = await request(server).get(`/api/tasks`)
     expect(res.statusCode).toBe(200)
     expect(res.body).toHaveLength(3)
   })
 
-  it('should return a task with a specific id', async () => {
+  it('should GET a task with a specific id', async () => {
     const res = await request(server).get(`/api/tasks/999`)
     expect(res.statusCode).toBe(200)
-    expect(res.body).toHaveLength(1)
-  })
-})
-
-describe('Test POST API calls', () => {
-  beforeAll(async () => {
-    await cleanup();
-  })
-
-  beforeEach(async () => {
-    await generateSampleData();
-  });
-
-  afterEach(async () => {
-    await cleanup();
-  })
-
-  it('should create a new task successfully', async () => {
+    expect(res.body).toMatchObject({
+      id: 999,
+      title: 'completed task'
+    })
+  }),
+  
+  it('should POST a new task successfully', async () => {
     const task = {
       title: 'new task',
       description: 'new description',
@@ -155,13 +135,39 @@ describe('Test POST API calls', () => {
     const res = await request(server).post('/api/create').send(task);
 
     expect(res.statusCode).toBe(200);
-    expect(res.body).toMatchObject([{
+    expect(res.body).toMatchObject({
       id: 1,
       title: 'new task',
       description: 'new description',
       status: 'TO_DO',
       dueDate: "2025-12-31T00:00:00.000Z",
       createdAt: "2020-01-01T00:00:00.000Z"
-    }]);
+    });
+  });
+  
+
+  it('should PUT a task successfully', async () => {
+    const task = {
+      title: 'changed name',
+      description: 'changed desc',
+      status: 'COMPLETED',
+      dueDate: new Date('2025-01-01'),
+    }
+
+    const res = await request(server).put('/api/tasks/999').send(task);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toMatchObject({
+      id: 999,
+      title: task.title,
+      description: task.description,
+      status: task.status,
+      dueDate: task.dueDate.toISOString(),
+    });
+  });
+
+  it('should DELETE a task successfully', async () => {
+    const res = await request(server).delete('/api/tasks/999');
+    expect(res.statusCode).toBe(200);
   });
 });
